8370260ed87dd5e3b27b6153aa9e1c09c450d85f
Md Reyaz Alam <76590179+reyaz-mra@users.noreply.github.com>
2022-07-21 09:01:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,14 +58,14 @@ app.use((req, res, next) => {
 // middleware function to check for logged-in users
 var sessionChecker = (req, res, next) => {
   if (req.session.user && req.cookies.user_sid) {
-    res.redirect("/");
+    res.redirect("/index");
   } else {
     next();
   }
 };
 var sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
-      res.redirect("/");
+      res.redirect("/index");
     } else {
       next();
     }
